e92ae8097860694c38070672981090677fc6fffe
Michael Rosata <mrosata1984@gmail.com>
2018-02-08 13:25:35 -0500

    fixed(syntax)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import FPP from "./components/FeaturedPricePlans";
-import FP from "./components/FeaturedPlan";
-import DPP from "./components/DetailedPricing";
+import FPP from "./components/FeaturedPricePlans"
+import FP from "./components/FeaturedPlan"
+import DPP from "./components/DetailedPricing"
 
 export const FeaturedPricingPlans = FPP
 export const DetailedPricing = DPP
